ff0ccf284e64c913749913f74de2d6b3105f1b86
Wictor Duarte <wictorduarte@outlook.com>
2025-02-14 20:32:54 -0300

    remove old structure folder

--- single-page/README.md
+++
@@ -1,3 +0,0 @@
-# Create a Ecommerce Single Web Page
-
-## This project is a model that simulates an ecommerce home page.
--- single-page/src/components/main/MosaicSection.js
+++
@@ -1,9 +0,0 @@
-function MosaicSection() {
-    return (
-        <div className="mosaic-section">
-            <h2>Mosaic Component Test</h2>
-        </div>
-    )
-}
-
-export default MosaicSection;
